efc4725616d695be779180e9c1001fe8c76c3abe
Jesus1911 <72622341+Jesus1911@users.noreply.github.com>
2021-06-16 13:08:23 -0500

    Update Matricesgeneralizadas2D3D.m

--- jesus/Matricesgeneralizadas2D3D.m
+++ jesus/Matricesgeneralizadas2D3D.m
@@ -3,10 +3,8 @@ clc; clear all; close all;
 %Red cuadrada 2D Generalizada nxm
 n=4; m=n;
 
-for i=1:n
-  r1(i)=i-1; r2(i)=i-1;
-end
-r11=repmat(r1,1,n); r22=repmat(r2,1,m);
+r1=0:n-1;
+r11=repmat(r1,1,n); r22=repmat(r1,1,m);
 R=[r11', sort(r22')];
 
 for i=1:n*m
@@ -19,14 +17,12 @@ for i=1:n*m
   end 
 end
 figure(1); plot(R(:,1),R(:,2), 'o'); 
-title('Arreglo generalizado 2D guías de ondas cuadradas');
+title('Arreglo generalizado 2D guÃ­as de ondas cuadradas');
 
-%Red cuadrada 3D Generalizada nxmxl
+%Red cubica 3D Generalizada nxmxl
 l=m;
 
-for i=1:n
-  r1(i)=i-1; 
-end
+r1=0:n-1;
 r111=repmat(r1,1,n*m); r221=sort(repmat(r1,1,m)); r222=repmat(r221,1,l);
 r333=repmat(r1,1,n*l); 
 R1=[r111', r222', sort(r333')];
@@ -41,5 +37,5 @@ for i=1:n*m*l
   end 
 end
 figure(2); hist3(R1(:,1),R1(:,2),R1(:,3), 'ob'); 
-title('Arreglo generalizado 3D guías de ondas cubicas');
+title('Arreglo generalizado 3D guÃ­as de ondas cubicas');
 
